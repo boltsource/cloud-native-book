e3cf6948abdedcdb24d1a0699280228b01b07bbf
Andrew Rhyne <rhyneandrew@gmail.com>
2019-06-13 01:26:08 -0500

    more cleanup

--- ops/terraform/memorystore.tf
+++ ops/terraform/memorystore.tf
@@ -1,17 +1,14 @@
 resource "google_project_service" "memorystore" {
-  service = "redis.googleapis.com"
-
-  disable_on_destroy         = true
-  disable_dependent_services = true
+  service                     = "redis.googleapis.com"
+  disable_on_destroy          = true
+  disable_dependent_services  = true
 }
 
 resource "google_redis_instance" "memorystore" {
-  name           = "cloud-memorystore"
-  tier           = "BASIC"
-  memory_size_gb = 1
-
-  depends_on = [google_project_service.memorystore]
-
-  location_id = "us-central1-a"
+  name            = "cloud-memorystore"
+  tier            = "BASIC"
+  memory_size_gb  = 1
+  depends_on      = [google_project_service.memorystore]
+  location_id     = "us-central1-a"
 }
 
